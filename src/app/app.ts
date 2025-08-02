4563a0dd02d77a0717dadf2e193fa3c2cc34ee6b
Abhinav Sinha <abhi.sinha1302@gmail.com>
2025-08-02 17:24:47 +0530

    feat: add cookie consent and some centralisation of css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { TodoList } from './components/todo-list/todo-list';
+import { Cookie } from './cookie/cookie';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, Header, Footer, TodoList],
+  imports: [FormsModule, Header, Footer, TodoList, Cookie],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
